3c5cef1f9924926980619931d904589e26d6fb11
Soham17102011 <115869174+Soham17102011@users.noreply.github.com>
2023-02-05 18:39:20 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 status = "";
+objects = [];
+input = "";
+
 
 function setup() {
     canvas = createCanvas(480, 380);
@@ -19,6 +22,40 @@ function modelLoaded() {
     status = true;
 }
 
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
 function draw() {
     image(video, 0, 0, 480, 380)
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+
+        for (i = 0; i < objects.length; i++) {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (input = objects[i].label) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = input + " is found";
+
+                synth = window.speechSynthesis;
+                speak_data = input + "is found";
+                utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+
+            } else {
+                document.getElementById("status").innerHTML = input + " not found";
+            }
+
+        }
+    }
 }
